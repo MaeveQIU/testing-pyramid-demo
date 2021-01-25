efc4c76968909536ab54676361d8caeac481bbc3
Maeve <maeve.cs23@gmail.com>
2021-01-25 09:20:53 +0800

    [maeve]-add database configuration

--- TestingPyramid/Program.cs
+++ TestingPyramid/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
 
 namespace TestingPyramid
 {
@@ -10,8 +12,18 @@ namespace TestingPyramid
             CreateHostBuilder(args).Build().Run();
         }
 
-        static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
+        static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            var config = Fluently.Configure()
+                .Database(
+                    MySQLConfiguration.Standard
+                        .ConnectionString("server=localhost;port=3306;database=pyramid;user=root;password=pyramid")
+                )
+                .BuildConfiguration()
+                .BuildSessionFactory();
+            
+            return Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+        }
     }
 }
